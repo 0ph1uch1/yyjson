9413e929a66e0ef2076dd0aefd6118ceb0ccb63c
antares <antares@alyr.dev>
2024-05-18 14:45:27 +0800

    dev tools

---
+++ dev_tools/shell.nix
@@ -0,0 +1,40 @@
+let
+  pkgs = import <nixpkgs> { };
+  # define version
+  using_python = pkgs.python312;
+  # import required python packages
+  required_python_packages = import ./py_requirements.nix;
+  #
+  extra_search_directory = ".extra-stub-path";
+  pyenv = using_python.withPackages required_python_packages;
+in
+pkgs.mkShell {
+  packages = [
+    pyenv
+    pkgs.cmake
+    pkgs.gdb
+    pkgs.valgrind
+  ];
+  shellHook = ''
+    if [[ ! -d ${extra_search_directory} ]]; then mkdir ${extra_search_directory}; fi
+    ensure_symlink() {
+        local link_path="$1"
+        local target_path="$2"
+        if [[ -L "$link_path" ]] && [[ "$(readlink "$link_path")" = "$target_path" ]]; then
+            return 0
+        fi
+        rm -f "$link_path" > /dev/null 2>&1
+        ln -s "$target_path" "$link_path"
+    }
+
+    for file in ${pyenv}/${using_python.sitePackages}/*; do
+        ensure_symlink ${extra_search_directory}/$(basename $file) $file
+    done
+    for file in ${extra_search_directory}/*; do
+        if [[ -L "$file" ]] && [[ "$(dirname $(readlink "$file"))" != "${pyenv}/${using_python.sitePackages}" ]]; then
+            rm -f "$file"
+        fi
+    done
+    ensure_symlink ${extra_search_directory}/python ${using_python}/bin/python
+  '';
+}
